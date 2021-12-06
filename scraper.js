c67e6491491b8f2dc3843b8977e6f85894e14aaa
Chris Ramesh <chris.ramesh@gmail.com>
2021-12-05 17:27:17 -0800

    finished scraper

--- scraper.js
+++ scraper.js
@@ -4,42 +4,51 @@ const cheerio = require("cheerio");
 
 // scraper
 const primalScrape = async () => {
-  // fetch data
-  const { data } = await axios.get(targetURL);
-  const $ = cheerio.load(data);
+  try {
+    // fetch data
+    const { data } = await axios.get(targetURL);
+    const $ = cheerio.load(data);
 
-  /* ++++++++ BOILER PLATE ++++++++++ */
-  const dataCenterGroupName = (dataCenter) => {
-    return $(dataCenter).find("h2").text();
-  };
-
-  const serverStatus = (server) => {
-    return {
-      name: $(server).find(".world-list__world_name p").text(),
-      allowsNewChars: !$(server)
-        .find(".world-list__create_character i")
-        .hasClass("world-ic__unavailable"),
+    /* ++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* ++++++++++++++++ BOILER PLATE ++++++++++++++++ */
+    /* ++++++++++++++++++++++++++++++++++++++++++++++ */
+    const dataCenterGroupName = (dataCenter) => {
+      return $(dataCenter).find("h2").text();
     };
-  };
 
-  // create an array of all the servers
-  const dataCenterStatus = (dataCenter) => {
-    const servers = $(dataCenter).children().find("div.world-list__item");
-    let arr = [];
-    servers.map((i) => arr.push(serverStatus(servers.get(i))));
-    return {
-      name: dataCenterGroupName(dataCenter),
-      servers: arr,
+    const serverStatus = (server) => {
+      return {
+        name: $(server).find(".world-list__world_name p").text(),
+        allowsNewChars: !$(server)
+          .find(".world-list__create_character i")
+          .hasClass("world-ic__unavailable"),
+      };
     };
-  };
 
-  /* ++++++++ SCRAPING LOGIC ++++++++++ */
-  // identify data centers
-  const allDataCenters = $("li.world-dcgroup__item");
-  let primal = allDataCenters.get(4);
+    // create an array of all the servers
+    const dataCenterStatus = (dataCenter) => {
+      const servers = $(dataCenter).children().find("div.world-list__item");
+      let arr = [];
+      servers.map((i) => arr.push(serverStatus(servers.get(i))));
+      return {
+        name: dataCenterGroupName(dataCenter),
+        servers: arr,
+      };
+    };
 
-  // return status object
-  return dataCenterStatus(primal);
+    /* ++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* ++++++++++++++++ SCRAPING LOGIC ++++++++++++++++ */
+    /* ++++++++++++++++++++++++++++++++++++++++++++++ */
+    // identify data centers
+    const allDataCenters = $("li.world-dcgroup__item");
+    let primal = allDataCenters.get(4);
+
+    // return status object
+    return dataCenterStatus(primal);
+  } catch (e) {
+    console.error("Unable to fetch website for scraping.");
+    return {};
+  }
 };
 
 module.exports = primalScrape;
--- server.js
+++ server.js
@@ -1,20 +1,69 @@
-// 3rd party
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++ 3RD PARTY ++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
 const express = require("express");
 const app = express();
 const axios = require("axios");
 const cheerio = require("cheerio");
+var cron = require("node-cron");
 
-// custom imports
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++ CUSTOM IMPORTS ++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
 const { port } = require("./config");
 const primalScrape = require("./scraper");
 
-// server
-const scrape = async () => {
-  const res = await primalScrape();
-  console.log(res);
-};
-scrape();
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++ CRON JOB ++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
 
+const check = () => {
+  console.log("Starting the cron job");
+  cron.schedule("*/15 * * * * *", async () => {
+    const webhookUrl =
+      "https://discord.com/api/webhooks/917217028610351165/hKgCnLc7RHLl6ikAVnAheTKqxi1PFRbgHV0fgYSH68CfmDThF1WMynyaDM_N8b1u9GYs";
+
+    // get latest server status
+    const status = await primalScrape();
+    console.log(status);
+
+    try {
+      // if error
+      if (!status.name) {
+        axios.post(webhookUrl, {
+          content:
+            "<@301969677100515328>, your shitty scraper broke. get rekt.",
+        });
+      }
+
+      // send a webhook if primal is open
+      const behemoth = status.servers.filter((i) => i.name == "Behemoth");
+      if (behemoth && behemoth.allowsNewChars) {
+        axios.post(webhookUrl, {
+          content:
+            "<@301969677100515328>, Behemoth is open for new characters! Sign up asap!",
+          embeds: [
+            {
+              title: "Primal Datacenter",
+              description: status.servers
+                .map((i) => `${i.allowsNewChars ? "🟢" : "🔴"} ${i.name} \n`)
+                .join(""),
+              url: "https://na.finalfantasyxiv.com/lodestone/worldstatus/",
+            },
+          ],
+        });
+      }
+    } catch (e) {
+      console.error("Issue with webhooks", e.message);
+    }
+  });
+};
+
+check();
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++ SERVER ++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++ */
 app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
